dc93e211cfccf8808a2cdaa5bad7cbf93452b437
mandi7469 <mandi7469@aol.com>
2024-07-23 18:52:07 -0400

    added starter files and folders and installed required dependencies

---
+++ index.js
@@ -0,0 +1,34 @@
+const inquirer = require('inquirer');
+const colors = require('colors');
+
+const questions = [
+    {
+        type: 'input',
+        message: 'Please enter up to three characters',
+        name: 'characters',
+      },
+      {
+        type: 'input',
+        message: 'Please enter a color for the text',
+        name: 'textColor',
+      },
+      {
+        type: 'list',
+        message: 'Please select a shape',
+        name: 'shape',
+        Choices: ['Circle', 'Triangle', 'Square']
+      },
+      {
+        type: 'input',
+        message: 'Please enter a color for the shape',
+        name: 'shapeColor',
+      },
+      
+];
+
+inquirer
+  .prompt(questions)
+  .then((response) =>{
+    let markDown = generateMarkdown(response)
+    writeToFile('README.md', markDown)
+  });
